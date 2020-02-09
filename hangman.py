0f3059be49c6dd00404feafb482cbd2c8d577134
Brendan Quirk <brendanquirk@Brendans-MacBook-Pro.local>
2020-02-08 19:59:32 -0700

    can push user guess into an array

--- hangman.py
+++ hangman.py
@@ -6,7 +6,7 @@ words_array = ['time', 'money', 'code', 'dragon', 'winning', 'avocado', 'waverid
 #hard coded word
 word = random.choice(words_array)
 #store user guesses (initially empty)
-guesses = ''
+guesses = []
 #user gets 10 turns
 turns = 10
 
@@ -17,25 +17,45 @@ name = input("What is your name?\n")
 print(f"Hello {name} time to play hangman!")
 
 time.sleep(1)
+print(f"The word has {len(word)} letters!")
+time.sleep(1)
 
-print("Guess a letter!")
+### Grab guess from user ###
+guess = input("Guess a letter!\n")
 time.sleep(.5)
 
+guesses.append(guess)
+
+print(guesses)
+
+guess = input("Guess a letter!\n")
+time.sleep(.5)
+
+guesses.append(guess)
+
+print(guesses)
 
 
 
-while turns > 0:
-    print(word)
-    turns -= 1
-    failed = 0
+### if guess isnt in guesses add to guesses ###
 
-    for char in word:
-        if char in guesses:
-            print(char),
-        else:
-            print("_")
-            failed += 1
+### if it is have them guess again ###
 
+### If guess isnt in word increment a failed variable ###
+
+
+# while turns > 0:
+#     print(word)
+#     turns -= 1
+#     failed = 0
+#
+#     for char in word:
+#         if char in guesses:
+#             print(char),
+#         else:
+#             print("_")
+#             failed += 1
+#
 
 #################### GRAVEYARD ####################
 
