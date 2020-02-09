c8908bc894c09ce71cd8e08055d8cd1ab1b880c5
Brendan Quirk <brendanquirk@Brendans-MacBook-Pro.local>
2020-02-08 19:50:39 -0700

    can select random word from array instead of having one word hard coded in

--- hangman.py
+++ hangman.py
@@ -1,4 +1,14 @@
 import time
+import random
+
+#word array to select from
+words_array = ['time', 'money', 'code', 'dragon', 'winning', 'avocado', 'waverider']
+#hard coded word
+word = random.choice(words_array)
+#store user guesses (initially empty)
+guesses = ''
+#user gets 10 turns
+turns = 10
 
 #grab user input from terminal
 name = input("What is your name?\n")
@@ -11,9 +21,25 @@ time.sleep(1)
 print("Guess a letter!")
 time.sleep(.5)
 
-#hard coded word
-word = 'secret'
-#store user guesses (initially empty)
-guesses = ''
-#user gets 10 turns
-turns = 10
+
+
+
+while turns > 0:
+    print(word)
+    turns -= 1
+    failed = 0
+
+    for char in word:
+        if char in guesses:
+            print(char),
+        else:
+            print("_")
+            failed += 1
+
+
+#################### GRAVEYARD ####################
+
+# def get_word():
+#     word = random.choice(words_array)
+#
+# get_word()
